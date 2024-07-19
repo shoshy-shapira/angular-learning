4df8189b57be79928eca19ed0f1b58e3a187dd7f
unknown <shf052@gmail.com>
2024-07-19 08:10:27 +0300

    print-routerWithParmes

--- src/app/employee-page/employee-page.component.ts
+++ src/app/employee-page/employee-page.component.ts
@@ -1,20 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { take } from 'rxjs';
+import { EmployeeService } from '../services/EmployeeService';
 
 @Component({
   selector: 'app-employee-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './employee-page.component.html',
   styleUrl: './employee-page.component.scss'
 })
 export class EmployeePageComponent {
-  currentId=null;
-constructor(private myActiveRouter:ActivatedRoute){};
+  currentId=0;
+  data:any;
+constructor(private myActiveRouter:ActivatedRoute, private employeeSrv:EmployeeService){};
 
 ngOnInit(){
   this.myActiveRouter.params.pipe(take(1)).subscribe((parm:any)=>{
 this.currentId=parm.id;
+this.employeeSrv.getEmployeeById(this.currentId).pipe(take(1)).subscribe(data=>
+  {
+this.data=data;
+  })
   })}
 }
